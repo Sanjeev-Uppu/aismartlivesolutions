0b3c7f904e92014551b2de91746bc8e751953607
Sanjeev Uppu <uppusanjeev2005@gmail.com>
2026-05-22 12:14:16 +0530

    deployment changes

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,27 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import {
+  createRouter,
+  RouterProvider,
+} from "@tanstack/react-router";
+
 import { routeTree } from "./routeTree.gen";
 
-export const getRouter = () => {
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
-  const router = createRouter({
-    routeTree,
-    context: { queryClient },
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-  });
+export const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  scrollRestoration: true,
+});
 
-  return router;
-};
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+export function AppRouter() {
+  return <RouterProvider router={router} />;
+}
